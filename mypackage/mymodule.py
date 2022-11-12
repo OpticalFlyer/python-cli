489f2a91be3f96b72d58313d52e7e8ed37e134cf
Chris Bomar <113836739+OpticalFlyer@users.noreply.github.com>
2022-11-12 12:36:46 -0600

    Testing upgrade

--- mypackage/mymodule.py
+++ mypackage/mymodule.py
@@ -1,12 +1,11 @@
 def my_function():
-    print('hello from my_function')
+    print('hello from my_function 2')
 
 
 def another_function():
-    print('hello from another_function')
+    print('hello from another_function 2')
 
 
 if __name__ == "__main__":
     """This runs when you execute '$ python3 mypackage/mymodule.py'"""
     my_function()
-    
